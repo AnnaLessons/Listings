5035f4ba4806612ae8cf26c3514db8fca30a726f
vladimir <vladimirrudt@yandex.ru>
2019-06-25 19:27:51 +0700

    It's test commit

--- src/main.kt
+++ src/main.kt
@@ -2,17 +2,5 @@
 
 fun main(args: Array<String>) {
 
-    var k: Double
-    var x = 5.0
-    var m: Double
-
-    print("Введите значение k")
-    k = readLine()!!.toDouble()
-    print("Введите значение m")
-    m = readLine()!!.toDouble()
-
-    var y: Double
-    y = k * x + m
-
-    println("Значение у = $y")
+    
 }
